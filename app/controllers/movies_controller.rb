8403f3c9863e9175c8b5f2964fa1ba57a94371a4
Andre Stackhouse <captainstack@outlook.com>
2016-04-27 01:34:03 +0000

    Add movie model/table

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,18 @@
 class MoviesController < ApplicationController
-  before_action :set_api_key, only: [:show, :tmdb_query]
+  before_action :set_api_key, only: [:show, :create, :tmdb_query]
   def index
   end
   
-  def show
+  def create
     tmdb_result = Tmdb::Movie.detail(params[:id])
-    movie = {id: tmdb_result['id'], title: tmdb_result['title'], 
-      release_date: tmdb_result['release_date'], poster_path: tmdb_result['poster_path']}
-    @movie = Movie.new(movie)
+    params = filter_params(tmdb_result)
+    @movie = Movie.new(params)
+    @movie.save!
     render 'show'
+  end
+  
+  def show
+    @movie = find_or_create_by_id(params[:id])
   end
   
   def tmdb_query
@@ -18,5 +22,34 @@ class MoviesController < ApplicationController
   private
     def set_api_key
       Tmdb::Api.key(ENV['TMDB_API_KEY'])
+    end
+    
+    def movie_params
+      params.require(:movie).permit(:content, :picture)
+    end
+    
+    def filter_params(params)
+      permitted = Set.new %w(
+        id
+        imdb_id 
+        title 
+        release_date 
+        overview 
+        poster_path 
+        backdrop_path 
+        popularity 
+        adult
+      )
+      params.select do 
+        |k, v| permitted.include?(k)
+      end
+    end
+    
+    def find_or_create_by_id(id)
+      if Movie.exists?(id)
+        Movie.find(id)
+      else
+        create
+      end
     end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,2 @@
-class Movie
-  include ActiveModel::Validations
-  include ActiveModel::Conversion
-  extend ActiveModel::Naming
-  
-  attr_accessor :id, :title, :release_date, :poster_path
-  
-  def initialize(attributes = {})
-    attributes.each do |name, value|
-      send("#{name}=", value)
-    end
-  end
+class Movie < ActiveRecord::Base
 end
